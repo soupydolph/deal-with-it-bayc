476b83b3ef3423d7957fac79f3ada819cb53ddf7
soupydolphin <soupydolph@proton.me>
2022-07-01 18:54:44 -0700

    hopefully resolving build errors

--- src/screens/Generator.js
+++ src/screens/Generator.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import glassesImg from './../deal-with-it.png'
 import textImg from './../text.png'
